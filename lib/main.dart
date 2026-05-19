1a5de4cbad0ef652843a5a70688e5d18e7e382b9
TomFXE <tomasjzc563@gmail.com>
2026-05-19 12:51:40 -0600

    integracion-de-firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MaterialApp(home: Scaffold(body: Home())));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(home: Scaffold(body: Home())));
+}
 
 class Home extends StatefulWidget {
   const Home({super.key});
